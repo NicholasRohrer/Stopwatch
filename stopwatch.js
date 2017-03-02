41d7e829b0d6f0eb38553ab8f409de2ceac0db81
Nicholas Rohrer <nrohrer2@gmail.com>
2017-03-02 12:49:37 -0600

    setting up functions and variables for stopwatch object

---
+++ stopwatch.js
@@ -0,0 +1,17 @@
+function Stopwatch() {
+	// private variables
+	var currentTime = 0;
+	var interval;
+	var offset;
+
+	// private functions
+	function update() {}
+	function delta() {}
+	function formatTime() {}
+
+	// public
+	this.isOn = false;
+	this.start = function() {};
+	this.stop = function() {};
+	this.reset = function() {};
+}
