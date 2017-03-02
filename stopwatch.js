30aa516bb036619d5adbfc2251eb11cea9b963e5
Nicholas Rohrer <nrohrer2@gmail.com>
2017-03-02 12:56:06 -0600

    finished setting up start, stop, and reset public functions

--- stopwatch.js
+++ stopwatch.js
@@ -11,7 +11,26 @@ function Stopwatch() {
 
 	// public
 	this.isOn = false;
-	this.start = function() {};
-	this.stop = function() {};
-	this.reset = function() {};
+	this.start = function() {
+		// only run if time is not currently running
+		if (this.isOn == false) {
+			interval = setInterval(update, 10);
+			offset = Date.now();
+			this.isOn = true;
+		}
+	};
+
+	this.stop = function() {
+		// only run if time is currently running
+		if (this.isOn == true) {
+			clearInterval(interval);
+			interval = null;
+			this.isOn = false;
+		}
+	};
+
+	this.reset = function() {
+		// re-initialize time to 0
+		currentTime = 0;
+	};
 }
