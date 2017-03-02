5233db6f5703ef206fb673c61a9e431f031d9aeb
Nicholas Rohrer <nrohrer2@gmail.com>
2017-03-02 17:22:07 -0600

    formatting for buttons

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 var $time = $('#time');
 var $start = $('#start');
 var $reset = $('#reset');
-var $message = $('#start-message');
 
 // create new stopwatch object
 var watch = new Stopwatch($time);
@@ -13,12 +12,12 @@ $start.on('click', function() {
 	// check if watch is on, start if not on
 	if (watch.isOn == true) {
 		watch.stop();
-		$message.html('START');
+		$start.html('START');
 	}
 
 	else {
 		watch.start();
-		$message.html('STOP');
+		$start.html('STOP');
 	}
 });
 
